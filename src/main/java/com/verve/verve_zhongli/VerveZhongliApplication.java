eb4ce6914f310b2a2c94d59478adabbcf883053b
zhonglishen <szl921818@gmail.com>
2024-09-16 22:35:12 +0200

    Project init

---
+++ src/main/java/com/verve/verve_zhongli/VerveZhongliApplication.java
@@ -0,0 +1,13 @@
+package com.verve.verve_zhongli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerveZhongliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerveZhongliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/verve/verve_zhongli/VerveZhongliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verve.verve_zhongli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerveZhongliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
